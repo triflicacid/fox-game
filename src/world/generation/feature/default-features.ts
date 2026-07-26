2ddd511d8096e31cbf85270b3f89f9c81d6dbe1f
RubenSaunders <ruben.saunders@fx.sgx.com>
2026-07-26 11:40:05 +0100

    Add oases

--- src/world/generation/feature/default-features.ts
+++ src/world/generation/feature/default-features.ts
@@ -1,7 +1,9 @@
 import {FeatureProvider} from "./feature";
 import {LakeFeature} from "./lakes";
+import {OasisFeature} from "./oases";
 
 /** Every feature a `ChunkGenerator` applies unless told otherwise. */
 export const DEFAULT_FEATURE_PROVIDERS: readonly FeatureProvider[] = [
     ({worldSeed, climate, terrainDepth}) => new LakeFeature(worldSeed, climate.moisture, terrainDepth),
+    ({worldSeed, terrainDepth}) => new OasisFeature(worldSeed, terrainDepth),
 ];
