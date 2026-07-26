d186e145c67679b83218328bdb7cd3bb9ce1635e
triflicacid <rubensaunders@hotmail.co.uk>
2026-07-26 22:55:22 +0100

    Added aliases to test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
 import {defineConfig} from "vitest/config";
+import path from "path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@display": path.resolve(__dirname, "lib/display/src"),
+            "@frames": path.resolve(__dirname, "lib/frames/src"),
+            "@keyboard": path.resolve(__dirname, "lib/keyboard/src/index.ts"),
+            "@lib": path.resolve(__dirname, "lib"),
+        },
+    },
     test: {
         name: "unit",
         environment: "node",
